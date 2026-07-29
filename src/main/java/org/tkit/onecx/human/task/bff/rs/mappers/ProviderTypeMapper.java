a0fff2be0853c98c3e253990b8f57862d9ecaa99
Wiktor Jordeczka <wiktor.jordeczka@capgemini.com>
2026-07-29 13:56:40 +0200

    feat: remove annotation

--- src/main/java/org/tkit/onecx/human/task/bff/rs/mappers/ProviderTypeMapper.java
+++ src/main/java/org/tkit/onecx/human/task/bff/rs/mappers/ProviderTypeMapper.java
@@ -1,6 +1,5 @@
 package org.tkit.onecx.human.task.bff.rs.mappers;
 
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.ValueMapping;
 import org.mapstruct.ValueMappings;
@@ -11,13 +10,11 @@ import gen.org.tkit.onecx.human.task.client.model.ProviderType;
 
 @Mapper(uses = { OffsetDateTimeMapper.class })
 public interface ProviderTypeMapper {
-    @BeanMapping(ignoreByDefault = true)
     @ValueMappings({
             @ValueMapping(source = "N8_N", target = "N8N")
     })
     ProviderType toBackend(ProviderTypeDTO source);
 
-    @BeanMapping(ignoreByDefault = true)
     @ValueMappings({
             @ValueMapping(source = "N8N", target = "N8_N")
     })
